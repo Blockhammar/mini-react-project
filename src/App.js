6c2a47fcb0110fce1126ebbfea50a6a6c2358935
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-02 10:17:34 +0100

    Uppdateringar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-
-function Allt (props){
+function Allt(props) {
   const userName = props.username;
   const [points, setPoints] = useState(0);
-  
-  
+
+  function Cards(props) {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function handleClick() {
+      setIsFlipped(!isFlipped);
+    }
+
+    if (isFlipped) {
+      return (
+        <div key="front" onClick={handleClick} style={{ backgroundColor: "blue", width: "150px", height: "150px" }}>
+          <img src={props.img} alt={props.name} />
+        </div>
+      );
+    } else {
+      <div key="back" onClick={handleClick} style={{ backgroundColor: "red", width: "150px", height: "150px" }}>
+      </div>;
+    }
+  }
 }
-
-
-
-
 
 function App() {
   return (
     <div className="App">
-      <Allt/>
+      <Allt />
     </div>
   );
 }
