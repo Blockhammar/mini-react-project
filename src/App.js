29b480a4e0c1fc8309d2112335b28f4991ab446b
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-02 09:43:29 +0100

    Grund

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
+function Allt (props){
+  const userName = props.username;
+  const [points, setPoints] = useState(0);
+  
+  
+}
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <p>HOLA</p>
+      <Allt/>
     </div>
   );
 }
